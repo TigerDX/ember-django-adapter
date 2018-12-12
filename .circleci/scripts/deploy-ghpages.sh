86119f5e17c7fe37717276dcac98cbcd3e567450
Benjamin Murden <ben.murden@gmail.com>
2018-12-12 11:08:22 +0900

    Uncomment deploy lines

--- .circleci/scripts/deploy-ghpages.sh
+++ .circleci/scripts/deploy-ghpages.sh
@@ -14,7 +14,7 @@ remote=$(git config remote.origin.url)
 siteSource=$1
 echo ${siteSource}
 
-# make a directory to put the gp-pages branch
+# make a directory to put the gh-pages branch
 mkdir gh-pages-branch
 cd gh-pages-branch
 # now lets setup a new repo so we can update the gh-pages branch
@@ -56,14 +56,14 @@ echo "Remove old files..."
 echo "done."
 
 # stage any changes and new files
-#git add -A
+git add -A
 # now commit, ignoring branch gh-pages doesn't seem to work, so trying skip
-#git commit --allow-empty -m "Deploy to GitHub pages [ci skip]"
+git commit --allow-empty -m "Deploy to GitHub pages [ci skip]"
 # and push, but send any output to /dev/null to hide anything sensitive
-#git push --force --quiet origin gh-pages
+git push --force --quiet origin gh-pages
 # go back to where we started and remove the gh-pages git repo we made and used
 # for deployment
-#cd ..
-#rm -rf gh-pages-branch
+cd ..
+rm -rf gh-pages-branch
 
 echo "Finished Deployment!"
